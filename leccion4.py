89d6ae722ee9ebe4d1788958b660e919c7dc7c29
M3dina <daniel_medina_garcia@hotmail.com>
2021-11-13 01:34:01 +0100

    Práctica Lección 04 (con Documentación)

--- leccion4.py
+++ leccion4.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pdb
 
-# TODO: Falta comentar los métodos
 
 def es_primo(n):
+    """
+        Método es_primo, realiza la comprobación de que el entero facilitado por parámetro es primo.
+        Parámetros:
+            n -> entero
+    """
     primo = True
     for i in range(2, n):
         if(n % i == 0):
@@ -12,16 +16,31 @@ def es_primo(n):
 
 
 def maxList(listas):
+    """
+        Método maxList, que dado una lista de listas de n números devuelva el máximo valor de cada una de las listas.
+        Parámetros:
+            listas -> lista de listas de enteros
+    """
     maxValueList = [j for i, j in enumerate(listas) if j == np.max(listas)]
     print(f'Los máximos de cada una de las listas pasadas por parámetro son: {maxValueList}')
 
 
 def filterPrimo(lista):
+    """
+        Método filterPrimo, que dado una lista de n números devuelva aquellos que son primos.
+        Parámetros:
+            lista -> lista de enteros
+    """
     primos = list(filter(es_primo, lista))
     print(f'Estos son los primos encontrados en la lista pasada por parámetro: {primos}')
 
 
 def init():
+    """
+        Método init(), es la función principal de la clase dónde se definirán
+                       las variables locales para llamar al resto de funciones
+                       definidas, maxList() y filterPrimo().
+    """
     pdb.set_trace()
     # Variables locales
     # Lista de listas para llamar a maxList()
